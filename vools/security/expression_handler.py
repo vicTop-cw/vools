f4394450de817b7862bda401c804bdc6e14de858
VictorTop <271390645@qq.com>
2026-06-04 16:35:02 +0800

    new file:   vools/security/expression_handler.py

---
+++ vools/security/expression_handler.py
@@ -0,0 +1,23 @@
+"""
+安全表达式处理模块
+提供安全的字符串表达式到函数的转换能力
+"""
+
+import ast
+import operator
+from typing import Any, Callable, Dict, Optional, Tuple, Union
+
+
+ALLOWED_OPERATORS = {
+    ast.Add: operator.add,
+    ast.Sub: operator.sub,
+    ast.Mult: operator.mul,
+    ast.Div: operator.truediv,
+    ast.FloorDiv: operator.floordiv,
+    ast.Mod: operator.mod,
+    ast.Pow: operator.pow,
+    ast.Lt: operator.lt,
+    ast.LtE: operator.le,
+    ast.Gt: operator.gt,
+    ast.GtE: operator.ge,
+    ast.Eq:
